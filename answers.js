ccbb15e1aa31ed06e69327bfcc53bead704fbe6c
Marie Godon <mariefrance.godon@gmail.com>
2016-04-13 17:36:26 +0000

    created lastChar function

--- answers.js
+++ answers.js
@@ -1,11 +1,21 @@
-//My answers to the workshop questions
+//my answers to the workshop questions
 
-//Function that returns first character of a string
+//function that returns first character of a string
 
 function firstChar(input) {
-        return input.substring(0, 1);
+    return input.substring(0, 1);
 }
 
 console.log(firstChar("string")); //s
-console.log(firstChar("")); //no output
-console.log(firstChar(223)); //error
+console.log(firstChar("")); //empty string length 1 char
+//console.log(firstChar(223)); error
+
+//function that takes string and returns last character
+
+function lastChar(input) {
+    var length = input.length;
+    return input.substring(length-1, length);
+}
+
+console.log(lastChar("string")); //g
+console.log(lastChar("")); //empty string length 1 char
