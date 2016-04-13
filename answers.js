18a9ef02aef4e5496eaa10af8f7fa978937d828d
Marie Godon <mariefrance.godon@gmail.com>
2016-04-13 17:24:56 +0000

    created firstChar function

---
+++ answers.js
@@ -0,0 +1,11 @@
+//My answers to the workshop questions
+
+//Function that returns first character of a string
+
+function firstChar(input) {
+        return input.substring(0, 1);
+}
+
+console.log(firstChar("string")); //s
+console.log(firstChar("")); //no output
+console.log(firstChar(223)); //error
